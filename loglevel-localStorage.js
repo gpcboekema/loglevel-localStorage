f040518707b044f9a67965b9790b9cf5bebacc7e
Gijs Boekema <gboekema@cyclomedia.com>
2016-07-15 16:00:45 +0200

    storing into localStorage

--- loglevel-localStorage.js
+++ loglevel-localStorage.js
@@ -1,65 +1,73 @@
-// 'use strict';
-//
-// /**
-//  * @description
-//  * Extend loglevel with new plugin which will send log information to the log-sever
-//  *
-//  * @param {object} logger loglevel instance to be extended
-//  * @param {object} options
-//  * @param {string} [options.url='http://localhost:8000/main/log'] Server url which would be used as a log server
-//  * @param {string|Function} [options.prefix=null] Prefix for all log messages. Either string or function wich should return string and accept log severity and message as parameters
-//  * @param {string|number} [options.level=logger.levels.WARN] Log level. Either string or number, {@see logger.setLevel}.
-//  * @param {boolean} [options.callOriginal=false] If set to true - original loglevel method for logging would be called
-//  * @example
-//  * loglevelLocalStorage(log,{url:'https://example.com/app/log',prefix: function(logSev,message) {
-//  *     return '[' + new Date().toISOString() + '] ' + logSev + ': ' + message + '\n'
-//  * }})
-//  */
-//
-// var loglevelLocalStorage = function loglevelLocalStorage(logger, options) {
-//     if (!logger || !logger.methodFactory) throw new Error('loglevel instance has to be specified in order to be extended');
-//
-//     var _logger = logger,
-//         _url = options && options.url || 'http://localhost:8000/main/log',
-//         _callOriginal = options && options.callOriginal || false,
-//         _prefix = options && options.prefix,
-//         _level = options && options.level || _logger.levels.WARN,
-//         _originalFactory = _logger.methodFactory,
-//         _sendQueue = [],
-//         _isSending = false;
-//
-//     _logger.methodFactory = function (methodName, logLevel) {
-//         var rawMethod = _originalFactory(methodName, logLevel);
-//
-//         return function (message) {
-//             if (typeof _prefix === 'string') message = _prefix + message;else if (typeof _prefix === 'function') message = _prefix(methodName, message);else message = methodName + ': ' + message;
-//
-//             if (_callOriginal) rawMethod(message);
-//
-//             _sendQueue.push(message);
-//             _sendNextMessage();
-//         };
-//     };
-//     _logger.setLevel(_level);
-//
-//     var _sendNextMessage = function _sendNextMessage() {
-//         if (!_sendQueue.length || _isSending) return;
-//
-//         _isSending = true;
-//
-//         var msg = _sendQueue.shift(),
-//             req = new XMLHttpRequest();
-//
-//         req.open("POST", _url, true);
-//         req.setRequestHeader('Content-Type', 'text/plain');
-//         req.onreadystatechange = function () {
-//             if (req.readyState == 4) {
-//                 _isSending = false;
-//                 setTimeout(_sendNextMessage, 0);
-//             }
-//         };
-//         req.send(msg);
-//     };
-// };
-//
-// module.exports = loglevelLocalStorage;
+import _ from 'lodash';
+/**
+ * @description
+ * Extend loglevel with new plugin which will send log information to the log-sever
+ *
+ * @param {object} logger loglevel instance to be extended
+ * @param {object} options
+ * @param {string} [options.storeKey='loglevelLocalStorage'] Name for the store in localStorage.
+ * @param {number} [options.maxLogStackSize=100] Max stack size to store.
+ * @param {boolean} [options.callOriginal=false] If set to true - original loglevel method for logging would be called
+ * @param {string|Function} [options.prefix=null] Prefix for all log messages. Either string or function wich should return string and accept log severity and message as parameters
+ * @param {string|number} [options.level=logger.levels.WARN] Log level. Either string or number, {@see logger.setLevel}.
+ * @example
+ * loglevelLocalStorage(log,{level:'info',prefix: function(logSev,message) {
+ *     return '[' + new Date().toISOString() + '] ' + logSev + ': ' + message + '\n'
+ * }})
+ */
+
+var loglevelLocalStorage = function loglevelLocalStorage(logger, options) {
+    if (!logger || !logger.methodFactory) throw new Error('loglevel instance has to be specified in order to be extended');
+
+    var _logger = logger,
+        _storeKey = options && options.storeKey || 'loglevelLocalStorage',
+        _maxLogStackSize = options && options.maxLogStackSize || 100,
+        _callOriginal = options && options.callOriginal || false,
+        _prefix = options && options.prefix,
+        _level = options && options.level || _logger.levels.WARN,
+        _originalFactory = _logger.methodFactory,
+        _logStack = [],
+        _logStackLocalStorage = [];
+
+    _logger.methodFactory = function (methodName, logLevel) {
+        var rawMethod = _originalFactory(methodName, logLevel);
+
+        return function (message) {
+            if (typeof _prefix === 'string') {
+                message = _prefix + message;
+            }
+            else if (typeof _prefix === 'function') {
+                message = _prefix(methodName, message);
+            }
+            else {
+                message = methodName + ': ' + message;
+            }
+
+            if (_callOriginal) {
+                rawMethod(message);
+            }
+            // Push to stack
+            _logStack.push(message);
+
+            // Always store a max of {_maxLogStackSize} logs
+            _logStackLocalStorage = _logStack.slice(-_maxLogStackSize);
+
+            //store to localStorage.
+            _store();
+        };
+    };
+
+    // Set severity level.
+    _logger.setLevel(_level);
+
+    var _storeLogStack = function _storeLogStack() {
+        if (!_logStackLocalStorage.length) return;
+
+        localStorage.setItem(_storeKey, JSON.stringify(_logStackLocalStorage));
+    };
+
+    // Creates a debounced function that delays invoking func until after wait milliseconds have elapsed since the last time the debounced function was invoked;
+    var _store = _.debounce(_storeLogStack, 250);
+};
+
+export default loglevelLocalStorage;
